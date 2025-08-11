66292eebf7c6d2e09bd680b5d0d0ed2dd4a79926
Rishi <rishi.karthikeyan22@imperial.ac.uk>
2025-08-11 13:06:28 +0000

    Updated Installation File

--- install_mint.sh
+++ install_mint.sh
@@ -2,8 +2,13 @@
 
 conda env create --name mint --file=environment.yml
 
+# Install mmseqs2 inside the 'mint' environment
+conda run -n mint conda install -y -c bioconda -c conda-forge mmseqs2
+
 # Ensure pip operations run inside the 'mint' environment
 conda run -n mint pip uninstall -y torch
 conda run -n mint pip install torch
+conda run -n mint pip install fire
+conda run -n mint pip install wandb
 conda run -n mint pip install seaborn
 conda run -n mint pip install -e .
